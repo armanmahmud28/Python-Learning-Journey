f0d1dd158f7a8065ab3d841bf41cfb6038d135fc
armanmahmud28 <armanmahmud0028@gmail.com>
2025-11-19 20:22:35 +0600

    Add time and date file

---
+++ DateTime.py
@@ -0,0 +1,14 @@
+import datetime
+
+x = datetime.datetime.now()
+
+
+print(x)
+print(x.strftime("%a"))
+print(x.strftime("%A"))
+print(x.strftime("%b"))
+print(x.strftime("%B"))
+print(x.strftime("%y"))
+print(x.strftime("%Y"))
+print(x.strftime("%d"))
+print(x.strftime("%x"))
