1688186a739d52ade8b7b227c810617938f14e55
armanmahmud28 <armanmahmud0028@gmail.com>
2025-11-21 00:48:26 +0600

    Add more file

---
+++ OOPS/self.py
@@ -0,0 +1,15 @@
+
+class Person:
+    def __init__(self, name):
+        self.name = name
+
+    def greet(self):
+        return "Hello, " + self.name
+
+    def welcome(self):
+        message = self.greet()
+        print(message + "! Welcome to our website.")
+
+
+p1 = Person("Nahid")
+p1.welcome()
