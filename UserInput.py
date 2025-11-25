0c93cb1910b96fa10223278a8c1b0f7006407cbf
armanmahmud28 <armanmahmud0028@gmail.com>
2025-11-25 19:33:35 +0600

    practice user input

---
+++ UserInput.py
@@ -0,0 +1,13 @@
+name = input("Enter You Name: ")
+
+print(f"Hello {name}")
+
+print(type(name))
+age = int(input("Enter Your age: "))
+
+if age >= 18:
+    print("You are eligible to Vote.")
+
+else:
+    print("You are Not eligible to Vote.")
+print(type(age))
