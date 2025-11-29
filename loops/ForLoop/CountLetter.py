e5abc5c44dc74b9d3363e2fdbfe5c7647a2cca78
armanmahmud28 <armanmahmud0028@gmail.com>
2025-11-29 23:39:26 +0600

    Practice loop on python

---
+++ loops/ForLoop/CountLetter.py
@@ -0,0 +1,8 @@
+
+text = "Hello World"
+count = 0
+for char in text:
+    count += 1
+
+
+print("Length: ", count)
---
+++ loops/ForLoop/SumOfNum.py
@@ -0,0 +1,9 @@
+
+# Sum of numbers
+
+total = 0
+for num in range(1, 6):
+    total += num
+
+
+print(total)
