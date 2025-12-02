385ebda7e41d684eb5d49be3775dbc81a505118d
armanmahmud28 <armanmahmud0028@gmail.com>
2025-12-02 23:17:46 +0600

    practice file on python language

--- file.py
+++ file.py
@@ -1,5 +1,18 @@
-f = open("file.txt")
 
-print(f.read())
+import os
 
-f.close()
+with open("test.txt", "w") as f:
+    f.writelines(["Hello World!\n", "line2\n", "line3"])
+
+with open("test.txt", "r") as f:
+    n = f.read()
+
+print(n)
+
+
+file = "test.txt"
+
+if os.path.exists(file):
+    os.remove(file)
+else:
+    print("File not found!")
